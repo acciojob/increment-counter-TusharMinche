e1f2d156be6360e8b69dc99f4cc58fba5aad4eae
Tushar Minche <89399696+TusharMinche@users.noreply.github.com>
2025-08-05 19:53:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let p = document.getElementById("counter");
+
+let counter = 0;
+
+document.body
+
+function incrementFn(){
+	alert(counter++);
+	p.innerText = counter;
+}
